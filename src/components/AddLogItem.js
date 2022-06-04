6903b52c5891db878e2409f9943cb765317e5c5b
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-04 16:57:38 +0300

    Add AddLog component

---
+++ src/components/AddLogItem.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+const AddLogItem = () => {
+  const [text, setText] = useState('');
+  const [user, setUser] = useState('');
+  const [priority, setPriority] = useState('');
+
+  return (
+    <Card className="mt-5 mb-3">
+      <Card.Body>
+        <Form>
+          <Row className="my-3">
+            <Col className="mb-3">
+              <Form.Control
+                placeholder="Log"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+            </Col>
+            <Row>
+              <Col>
+                <Form.Control
+                  placeholder="User"
+                  value={user}
+                  onChange={(e) => setUser(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  as="select"
+                  value={priority}
+                  onChange={(e) => setPriority(e.target.value)}
+                >
+                  <option value="0"> Select Priority </option>
+                  <option value="Low"> Low </option>
+                  <option value="Moderate"> Moderate </option>
+                  <option value="High"> High </option>
+                </Form.Control>
+              </Col>
+            </Row>
+            <Row className="my-3">
+              <Col>
+                <Button type="submit" variant="secondary" block>
+                  Add Log
+                </Button>
+              </Col>
+            </Row>
+          </Row>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default AddLogItem;
