b8769c9699b789dfea6d3b254cea435ac64204bb
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-04 15:58:18 +0300

    Add Component

---
+++ src/components/LogItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LogItem = () => {
+  return (
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  );
+};
+
+export default LogItem;
