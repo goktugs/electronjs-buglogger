6edffc975c47ff45fcc391c12a5b0550048446b0
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-04 17:45:47 +0300

    Add Display Alerts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
+
 import LogItem from './LogItem';
 import AddLogItem from './AddLogItem';
 
@@ -29,14 +31,45 @@ const App = () => {
     },
   ]);
 
+  const [alert, setAlert] = useState({
+    show: false,
+    message: '',
+    variant: 'success',
+  });
+
   function addItem(item) {
+    if (item.text === '' || item.user === '' || item.priority === '') {
+      showAlert('Please enter all areas', 'danger');
+      return false;
+    }
+
     item._id = Math.floor(Math.random() * 90000) + 10000;
     item.created = new Date().toString();
     setLogs([...logs, item]);
+
+    showAlert('Log Added');
   }
+
+  function showAlert(message, variant = 'success', seconds = 3000) {
+    setAlert({
+      show: true,
+      message,
+      variant,
+    });
+
+    setTimeout(() => {
+      setAlert({
+        show: false,
+        message: '',
+        variant: 'success',
+      });
+    }, seconds);
+  }
+
   return (
     <Container>
       <AddLogItem addItem={addItem} />
+      {alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
       <Table>
         <thead>
           <tr>
