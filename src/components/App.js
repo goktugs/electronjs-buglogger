9d2381f6ed690b38c6a683ff52f566e58442f25e
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-04 15:57:58 +0300

    Start Project

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
+import LogItem from './LogItem';
+
+const App = () => {
+  const [logs, setLogs] = useState([
+    {
+      id: 1,
+      text: 'This is log one',
+      priority: 'low',
+      user: 'Brad',
+      created: new Date().toString(),
+    },
+    {
+      id: 2,
+      text: 'This is log two',
+      priority: 'moderate',
+      user: 'Kate',
+      created: new Date().toString(),
+    },
+    {
+      id: 3,
+      text: 'This is log three',
+      priority: 'high',
+      user: 'John',
+      created: new Date().toString(),
+    },
+  ]);
+
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log) => (
+            <LogItem />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default App;
